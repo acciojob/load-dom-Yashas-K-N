0b17038f4b02430314016d4e8f4af61dfd11da25
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2025-03-07 20:59:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",() => {
-	loadding.innerText = "DOM load success";
+	document.innerText = "DOM load success";
   }
 );
